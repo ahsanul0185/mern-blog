cd54e48787119a4c05fe08132122909cf8d2c63f
Ahsanul Haque <ahsanul0185@gmail.com>
2025-06-20 19:37:42 +0600

    db connection successfull

--- server/index.js
+++ server/index.js
@@ -1,5 +1,18 @@
 
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+ 
+mongoose.connect(process.env.DB_URL)
+    .then(() => {
+        console.log("db connected successfully")
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
 
 
 const app = express();
