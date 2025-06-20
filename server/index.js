d475317dfa3484ebfdcd30ce8b4eebd38134dd18
Ahsanul Haque <ahsanul0185@gmail.com>
2025-06-20 19:56:55 +0600

    created test api route

--- server/index.js
+++ server/index.js
@@ -1,28 +1,30 @@
-
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+import userRoutes from "./routes/user.route.js"
+
 dotenv.config();
- 
-mongoose.connect(process.env.DB_URL)
-    .then(() => {
-        console.log("db connected successfully")
-    })
-    .catch((err) => {
-        console.log(err)
-    })
 
-
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log("db connected successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-    res.json({message : "Welcome to the server - Blogs by Ahsanul"})
-})
-
+  res.json({ message: "Welcome to the server - Blogs by Ahsanul" });
+});
 
 app.listen(PORT, () => {
-    console.log(`server is running at http://localhost:${PORT}`);
-})
+  console.log(`server is running at http://localhost:${PORT}`);
+});
+
+
+app.use("/api/user", userRoutes)
